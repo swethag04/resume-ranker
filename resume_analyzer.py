6e175af5f71a5210140f22d696403f55e7580f0f
Swetha Ganapathi Raman <swethag04@gmail.com>
2024-04-20 17:22:08 -0700

    remove white line spaces

--- resume_analyzer.py
+++ resume_analyzer.py
@@ -6,7 +6,6 @@ import PyPDF2
 OPENAI_API_KEY = getpass()
 os.environ["OPENAI_API_KEY"]  = OPENAI_API_KEY
 
- 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains.summarize import load_summarize_chain
 from langchain_openai import ChatOpenAI
@@ -17,6 +16,7 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 
 def summarize_resumes(pdf_folder):
+    
     summaries = []
     for pdf_file in glob.glob(pdf_folder + "/*.pdf"):
         loader = PyPDFLoader(pdf_file) 
@@ -40,7 +40,6 @@ def summarize_resumes(pdf_folder):
        # print(summary)
        # print("\n")
         summaries.append(summary)
-
         
     return summaries
 
@@ -50,9 +49,10 @@ def extract_job_req(job_desc):
     desc = job_loader.load()
     prompt_template = """
             You are an expert extraction algorithm. 
-            Extract the key qualifications required for this role from the job description in JSON format.
-            The keys in the JSON are job title, experience, technical skills, social skills,degree.
-                       
+            Extract the key qualifications required for this role from the job description 
+            in JSON format.
+            The keys in the JSON are job title, experience, technical skills, social 
+            skills, degree.                       
         "{text}"
         OUTPUT: """
     prompt = PromptTemplate.from_template(prompt_template)
@@ -66,34 +66,37 @@ def extract_job_req(job_desc):
 def match_resumes(pdf_folder, job_desc_json):
     # Define prompt
         prompt_template = """ 
-        You are an expert recruiter specializing in  analyzing job descriptions and matching resumes.
-        Your role involves a meticulous process of evaluating resumes against specific job requirements.
+        You are an expert recruiter specializing in  analyzing job descriptions and 
+        matching resumes. Your role involves a meticulous process of evaluating resumes 
+        against specific job requirements.
 
         You are provided with resumes of multiple candidates  {text}
 
-        You are also provided with the description for the job being hired {job_desc_json}
+        You are also provided with  description for the job being hired {job_desc_json}
 
-        Review the job description noting job title, experience, technical skills, social skills, degree required for the job.
-        Each category is scored on a scale of 1 to 5, where 1 indicates a poor match and 5 indicates an excellent match.
+        Review the job description noting job title, experience, technical skills, social 
+        skills, degree required for the job. Each category is scored on a scale of 1 to 5, 
+        where 1 indicates a poor match and 5 indicates an excellent match.
 
-        Before assigning scores, take a moment to reflect on the candidate's overall profile in relation to the job description.
-        Evaluate the candidate's work history for relevance to the job's experience requirements, considering past roles, 
-        industries, and levels of responsibility.
-        Assess how the candidate's listed skills align with the job's requirements, including both hard and soft skills
+        Before assigning scores, take a moment to reflect on the candidate's overall profile 
+        in relation to the job description. Evaluate the candidate's work history for relevance 
+        to the job's experience requirements, considering past roles, industries, and levels of 
+        responsibility. Assess how the candidate's listed skills align with the job's requirements, 
+        including both hard and soft skills
 
-        Sum the scores to get a total that indicates the match level between the candidate's resume and the job description
+        Sum the scores to get a total that indicates the match level between the candidate's resume 
+        and the job description
 
         Rank all the resumes in descending order of scores and show only the top 2 matching candidates.
         
-
-         Final output should be generated in the below format as a JSON with the below keys:
+        Final output should be generated in the below format as a JSON with the below keys:
          
          'Name of the candidate': 
          'Title':
          'Current Employer':
          'Score for job title':
          'Score for experience':
-        'Score for social skills':
+         'Score for social skills':
          'Score for technical skills':
          'Score for degree':
          'Total score':
